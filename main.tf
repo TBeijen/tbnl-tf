fe8c49a141459988f3da3e8dc4a15ae5cc5e8514
Tibo Beijen <tibobeijen@gmail.com>
2023-09-17 08:53:04 +0200

    Setting up remote state resources

--- main.tf
+++ main.tf
@@ -18,17 +18,21 @@ locals {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket        = local.state_bucket
-  force_destroy = true
-  versioning {
-    enabled = true
-  }
+  force_destroy = false
+}
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
     }
   }
 }
